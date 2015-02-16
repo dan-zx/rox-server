55992cd0085086a811ae3c815b4920e7e35bc9b5
dan-zx <danyboy_shaggy@hotmail.com>
2015-02-16 13:04:03 -0600

    Added CredentialDaoTest class and general test set up.

---
+++ src/test/java/com/grayfox/server/config/TestConfig.java
@@ -0,0 +1,38 @@
+package com.grayfox.server.config;
+
+import static org.mockito.Mockito.spy;
+
+import com.foursquare4j.FoursquareApi;
+
+import com.google.maps.GeoApiContext;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:test-configs.properties")
+@Import({MainConfig.DataConfig.class, TestConfig.BeanConfig.class})
+public class TestConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Configuration
+    public static class BeanConfig extends MainConfig.BeanConfig {
+
+        @Override
+        public FoursquareApi foursquareApi(String clientId, String clientSecret) {
+            return spy(super.foursquareApi(clientId, clientSecret));
+        }
+
+        @Override
+        public GeoApiContext geoApiContext(String apiKey) {
+            return spy(super.geoApiContext(apiKey));
+        }
+    }
+}
---
+++ src/test/java/com/grayfox/server/dao/CredentialDaoTest.java
@@ -0,0 +1,46 @@
+package com.grayfox.server.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.inject.Inject;
+
+import com.grayfox.server.config.TestConfig;
+import com.grayfox.server.domain.Credential;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+@TransactionConfiguration(defaultRollback = true)
+public class CredentialDaoTest {
+
+    @Inject private CredentialDao credentialDao;
+
+    @Before
+    public void setUp() {
+        assertThat(credentialDao).isNotNull();
+    }
+
+    @Test
+    @Transactional
+    public void testSaveAndFetch() {
+        Credential expectedCredential = new Credential();
+        expectedCredential.setAccessToken("fakeAccessToken");
+        expectedCredential.setFoursquareAccessToken("fakeFoursquareAccessToken");
+        
+        assertThat(credentialDao.existsAccessToken(expectedCredential.getAccessToken())).isFalse();
+        assertThat(credentialDao.fetchByFoursquareAccessToken(expectedCredential.getFoursquareAccessToken())).isNull();
+
+        credentialDao.save(expectedCredential);
+
+        assertThat(credentialDao.existsAccessToken(expectedCredential.getAccessToken())).isTrue();
+        assertThat(credentialDao.fetchByFoursquareAccessToken(expectedCredential.getFoursquareAccessToken())).isNotNull().isEqualTo(expectedCredential);
+    }
+}
