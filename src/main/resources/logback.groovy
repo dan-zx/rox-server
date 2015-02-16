f6a748ea027dee824ddc8c7a7595de0f57237708
dan-zx <danyboy_shaggy@hotmail.com>
2015-02-15 23:18:13 -0600

    Format fixes. Gradle fixes. Note to self: JUL is shit

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,22 +1,24 @@
-appender("file", FileAppender) {
-  file = "log.txt"
-  append = false
-  encoder(PatternLayoutEncoder) {
-    pattern = "[%d{yyyy/MM/dd HH:mm:ss.SSS}] {%thread} %-5level in %logger: %msg%n"
-  }
+def logPattern = "[%d{yyyy/MM/dd HH:mm:ss.SSS}] {%thread} %-5level in %logger: %msg%n"
+
+appender('file', FileAppender) {
+    file = 'log.txt'
+    append = false
+    encoder(PatternLayoutEncoder) {
+        pattern = logPattern
+    }
 }
 
-appender("console", ConsoleAppender) {
-  encoder(PatternLayoutEncoder) {
-	pattern = "[%d{yyyy/MM/dd HH:mm:ss.SSS}] {%thread} %-5level in %logger: %msg%n"
-  }
+appender('console', ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = logPattern
+    }
 }
 
-logger("com.foursquare4j", WARN)
-logger("org.apache.http", WARN)
-logger("org.hibernate.validator", WARN)
-logger("org.jboss.logging", WARN)
-logger("org.neo4j", WARN)
-logger("org.springframework", WARN)
+logger('com.foursquare4j', WARN)
+logger('org.apache.http', WARN)
+logger('org.hibernate.validator', WARN)
+logger('org.jboss.logging', WARN)
+logger('org.neo4j', WARN)
+logger('org.springframework', WARN)
 
-root(ALL, ["console", "file"])
+root(ALL, ['console', 'file'])
---
+++ src/test/java/com/grayfox/server/DeleteMe.java
@@ -0,0 +1,3 @@
+package com.grayfox.server;
+
+class DeleteMe { }
