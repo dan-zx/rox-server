9ce4f0b8cb0b06f473f5992d8508464b34df363d
dan-zx <danyboy_shaggy@hotmail.com>
2015-02-16 16:40:50 -0600

    " to '

--- src/test/resources/logback.groovy
+++ src/test/resources/logback.groovy
@@ -12,18 +12,18 @@ context.resetJUL = true
 
 LogManager.getLogManager().reset()
 SLF4JBridgeHandler.install()
-Logger.getLogger("global").setLevel(FINEST)
+Logger.getLogger('global').setLevel(FINEST)
 
-appender("console", ConsoleAppender) {
+appender('console', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "[%d{yyyy/MM/dd HH:mm:ss.SSS}] {%thread} %-5level in %logger: %msg%n"
     }
 }
 
-logger("com.foursquare4j", WARN)
-logger("com.google.maps", WARN)
-logger("org.apache.http", WARN)
-logger("org.neo4j", WARN)
-logger("org.springframework", WARN)
+logger('com.foursquare4j', WARN)
+logger('com.google.maps', WARN)
+logger('org.apache.http', WARN)
+logger('org.neo4j', WARN)
+logger('org.springframework', WARN)
 
-root(ALL, ["console"])
+root(ALL, ['console'])
