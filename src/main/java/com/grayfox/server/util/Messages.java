ece9bdb37cf10617dae2e8696baaecbb62ecfb33
dan-zx <danyboy_shaggy@hotmail.com>
2015-04-05 21:01:42 -0500

    Messages enhancements.

--- src/main/java/com/grayfox/server/util/Messages.java
+++ src/main/java/com/grayfox/server/util/Messages.java
@@ -13,9 +13,7 @@ import org.slf4j.LoggerFactory;
 
 public final class Messages {
 
-    public static final Locale DEFAULT_LOCALE = new Locale("en");
-    public static final Collection<Locale> SUPPORTED_LOCALES = Collections.unmodifiableCollection(Arrays.asList(DEFAULT_LOCALE, new Locale("es")));
-
+    private static final Collection<Locale> SUPPORTED_LOCALES = Collections.singletonList(new Locale("es"));
     private static final String RESOURCE_BUNDLE_BASE_NAME = "com.grayfox.server.messages";
     private static final String MISSING_RESOURCE_KEY_FORMAT = "???%s???";
     private static final Logger LOGGER = LoggerFactory.getLogger(Messages.class);
@@ -26,7 +24,7 @@ public final class Messages {
 
     public static String get(String key) {
         try {
-            return ResourceBundle.getBundle(RESOURCE_BUNDLE_BASE_NAME, DEFAULT_LOCALE).getString(key);
+            return ResourceBundle.getBundle(RESOURCE_BUNDLE_BASE_NAME, Locale.ROOT).getString(key);
         } catch (MissingResourceException ex) {
             LOGGER.warn("Can't find message for key: [{}]", key, ex);
             return String.format(MISSING_RESOURCE_KEY_FORMAT, key);
@@ -35,7 +33,7 @@ public final class Messages {
 
     public static String get(String key, Locale locale) {
         try {
-            ResourceBundle bundle = SUPPORTED_LOCALES.contains(locale) ? ResourceBundle.getBundle(RESOURCE_BUNDLE_BASE_NAME, locale) : ResourceBundle.getBundle(RESOURCE_BUNDLE_BASE_NAME, DEFAULT_LOCALE);
+            ResourceBundle bundle = SUPPORTED_LOCALES.contains(locale) ? ResourceBundle.getBundle(RESOURCE_BUNDLE_BASE_NAME, locale) : ResourceBundle.getBundle(RESOURCE_BUNDLE_BASE_NAME, Locale.ROOT);
             return bundle.getString(key);
         } catch (MissingResourceException ex) {
             LOGGER.warn("Can't find message for key: [{}]", key, ex);
--- src/main/java/com/grayfox/server/ws/rest/BaseRestComponent.java
+++ src/main/java/com/grayfox/server/ws/rest/BaseRestComponent.java
@@ -5,8 +5,6 @@ import java.util.Locale;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 
-import com.grayfox.server.util.Messages;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,14 +17,11 @@ public abstract class BaseRestComponent {
     protected Locale getClientLocale() {
         if (headers != null && headers.getAcceptableLanguages() != null && headers.getAcceptableLanguages().size() > 0) {
             for (Locale locale : headers.getAcceptableLanguages()) {
-                if (locale != null && locale.getLanguage() != null) {
-                    LOGGER.debug("Client primary language: '{}'", locale.getLanguage());
-                    return new Locale(locale.getLanguage());
-                }
+                LOGGER.debug("Client primary language: '{}'", locale.getLanguage());
+                return new Locale(locale.getLanguage());
             }
         }
 
-        LOGGER.debug("Client languages not available. Using default language");
-        return Messages.DEFAULT_LOCALE;
+        return null;
     }
 }
