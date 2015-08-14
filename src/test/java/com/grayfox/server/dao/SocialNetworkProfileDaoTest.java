8c8b04f06d28800229bfe593c0a64653452d6c79
dan-zx <danyboy_shaggy@hotmail.com>
2015-08-14 14:47:12 -0500

    Complete SocialNetworkProfileDaoTest

--- src/test/java/com/grayfox/server/dao/SocialNetworkProfileDaoTest.java
+++ src/test/java/com/grayfox/server/dao/SocialNetworkProfileDaoTest.java
@@ -1,14 +1,21 @@
 package com.grayfox.server.dao;
 
 import static org.assertj.core.api.StrictAssertions.assertThat;
+import static org.assertj.core.api.StrictAssertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 import javax.inject.Inject;
 
-import org.junit.Test;
-
+import com.grayfox.server.domain.Category;
+import com.grayfox.server.domain.User;
 import com.grayfox.server.test.config.TestConfig;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -24,7 +31,31 @@ public class SocialNetworkProfileDaoTest {
     }
 
     @Test
-    public void collectUserDataTest() {
-        assertThat(socialNetworkProfileDao.collectUserData("fakeToken")).isNotNull();
+    public void testCollectUserData() {
+        User expectedUser = new User();
+        expectedUser.setName("John");
+        expectedUser.setLastName("Doe");
+        expectedUser.setPhotoUrl("https://irs3.4sqi.net/img/user/300x300/photo.jpg");
+        expectedUser.setFoursquareId("34468234");
+        Category selfLike = new Category();
+        selfLike.setFoursquareId("4bf58dd8d48988d190941735");
+        expectedUser.setLikes(new HashSet<>(Arrays.asList(selfLike)));
+        User friend = new User();
+        friend.setName("Jane");
+        friend.setLastName("Doe");
+        friend.setPhotoUrl("https://irs1.4sqi.net/img/user/300x300/photo.jpg");
+        friend.setFoursquareId("94578235");
+        Category friendLike = new Category();
+        friendLike.setFoursquareId("4bf58dd8d48988d175941735");
+        friend.setLikes(new HashSet<>(Arrays.asList(friendLike)));
+        expectedUser.setFriends(new HashSet<>(Arrays.asList(friend)));
+
+        assertThat(socialNetworkProfileDao.collectUserData("fakeToken")).isNotNull().isEqualTo(expectedUser);
+    }
+
+    @Test
+    public void testErrorInCollectUserData() {
+        assertThatThrownBy(() -> socialNetworkProfileDao.collectUserData("invalidToken"))
+            .isInstanceOf(DaoException.class);
     }
 }
--- src/test/java/com/grayfox/server/test/dao/foursquare/MockFoursquareProfileDao.java
+++ src/test/java/com/grayfox/server/test/dao/foursquare/MockFoursquareProfileDao.java
@@ -1,15 +1,43 @@
 package com.grayfox.server.test.dao.foursquare;
 
-import com.grayfox.server.dao.foursquare.FoursquareProfileDao;
+import javax.inject.Inject;
 
+import com.grayfox.server.test.util.HttpStatus;
+import com.grayfox.server.test.util.Utils;
+import com.squareup.okhttp.mockwebserver.MockResponse;
+
+import com.foursquare4j.FoursquareApi;
+import com.grayfox.server.dao.foursquare.FoursquareProfileDao;
 import com.grayfox.server.domain.User;
+import com.squareup.okhttp.mockwebserver.MockWebServer;
 import org.springframework.stereotype.Repository;
 
 @Repository("foursquareProfileDao")
 public class MockFoursquareProfileDao extends FoursquareProfileDao {
 
+    @Inject private MockWebServer mockWebServer;
+    @Inject private FoursquareApi foursquareApi;
+
     @Override
     public User collectUserData(String accessToken) {
-        return null;
+        if (!"fakeToken".equals(accessToken)) {
+            mockWebServer.enqueue(new MockResponse()
+                .setStatus(HttpStatus.OK.toString())
+                .setBody(Utils.getContentFromFileInClasspath("responses/generic_error.json")));
+        } else {
+            mockWebServer.enqueue(new MockResponse()
+                .setStatus(HttpStatus.OK.toString())
+                .setBody(Utils.getContentFromFileInClasspath("responses/user.json")));
+            mockWebServer.enqueue(new MockResponse()
+                .setStatus(HttpStatus.OK.toString())
+                .setBody(Utils.getContentFromFileInClasspath("responses/venuelikes_self.json")));
+            mockWebServer.enqueue(new MockResponse()
+                .setStatus(HttpStatus.OK.toString())
+                .setBody(Utils.getContentFromFileInClasspath("responses/friends.json")));
+            mockWebServer.enqueue(new MockResponse()
+                .setStatus(HttpStatus.OK.toString())
+                .setBody(Utils.getContentFromFileInClasspath("responses/venuelikes_friends.json")));
+        }
+        return collectUserData(foursquareApi);
     }
 }
